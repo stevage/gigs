2366818b483b46dd043d9f123a75b184710d0bd1
Steve Bennett <stevage@gmail.com>
2013-05-13 19:56:28 +1000

    Fix Internal Server Error message when user goes straight to a detail page without creating a search context.

--- gigs/search/templatetags/search_results.py
+++ gigs/search/templatetags/search_results.py
@@ -2,12 +2,19 @@ from django import template
 
 register = template.Library()
 
-
 @register.inclusion_tag('search/search_results.html', takes_context=True)
 def search_results(context):
-    request = context['request']
-    selected_facets = request.session.get('selected_facets', None)
-    # TODO add query to search url resolved by name
-    return {
-        'selected_facets': selected_facets
-    }
+    try:
+        request = context['request']
+        selected_facets = request.session.get('selected_facets', None)
+        # TODO add query to search url resolved by name
+        return {
+            'selected_facets': selected_facets
+        }
+    except KeyError:
+        # No context? User went straight to page URL without searching.
+        return {
+            'selected_facets': {
+                'render_url': ''
+            }
+        }
